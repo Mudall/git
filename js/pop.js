96284da971bdcd06c846fa5221be06593fca78c3
Mudall <muhyeon04@gamil.com>
2025-07-02 17:32:21 +0900

    .

--- js/pop.js
+++ js/pop.js
@@ -1,3 +1,5 @@
+
+// 팝업 온오프
 document.addEventListener("DOMContentLoaded", function () {
     const openBtn = document.querySelector(".btn_pop_open");
     const popup = document.getElementById("popupLayer");
@@ -15,3 +17,29 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+
+
+
+// 색인 검색
+    const buttons = document.querySelectorAll('.index-buttons button');
+    const lists = document.querySelectorAll('.index-list');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+        // 활성 버튼 표시
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        const idx = button.dataset.index;
+
+        // 해당 색인 리스트만 보이게, 나머지 숨기기
+        lists.forEach(list => {
+            if(list.dataset.indexList === idx) {
+            list.hidden = false;
+            } else {
+            list.hidden = true;
+            }
+        });
+        });
+    });
